d9c4de679521a173deef4ec2ea8715242c7f7167
Vito De Tullio <vito.detullio@finconsgroup.com>
2020-04-15 11:41:29 +0200

    enable work in place

--- src/__main__.py
+++ src/__main__.py
@@ -10,10 +10,15 @@ def main() -> None:
         raise SystemExit(f'uso: {sys.argv[0]} ACCUMULATOR [MOVIMENTI...]')
 
     accumulator, *movimentis = sys.argv[1:]
-    in_place = False
+    in_place = True
     suffix = '~'
 
     Merger(suffix, in_place=in_place).merge(accumulator, *movimentis)
+
+    print(f'overridden {accumulator}', file=sys.stderr)
+    print(f'backupd at {accumulator}{suffix}', file=sys.stderr)
+    for movimenti in movimentis:
+        print(f'and merged {movimenti}', file=sys.stderr)
 
 
 if __name__ == '__main__':

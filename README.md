04dfbe7a962b10ec7ae9db7eb8fcd711162c931a
Vito De Tullio <vito.detullio@gmail.com>
2022-11-05 09:55:46 +0100

    cleanups

--- README.md
+++
@@ -1,3 +0,0 @@
-# movs-merger
-
-![Code scanning - action](https://github.com/ZeeD/movs-merger/workflows/Code%20scanning%20-%20action/badge.svg)
--- setup.py
+++ setup.py
@@ -3,16 +3,10 @@ from setuptools import setup
 
 setup(name='movs-merger',
       version='0.0.0',
-      url='http://www.example.com',
+      url='https://github.com/ZeeD/movs-merger',
       author='Vito De Tullio',
       author_email='vito.detullio@gmail.com',
       py_modules=find_packages(),
-      extra_require={
-          'dev': [
-              'distutil'
-          ]
-      },
-      package_dir={'': 'src'},
       package_data={
           'movs': ['py.typed'],
       },

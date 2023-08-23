91cc15fb99258bb3e41f500f63bd5d78f9144825
Vito De Tullio <vito.detullio@gmail.com>
2023-08-23 09:35:53 +0200

    support postepay

--- movsmerger/__init__.py
+++ movsmerger/__init__.py
@@ -3,12 +3,14 @@ from difflib import SequenceMatcher
 from shutil import move
 from sys import argv
 from sys import stderr
+from typing import Callable
 
 from movs import read_txt
 from movs import write_txt
 from movs.estrattoconto import read_estrattoconto
 from movs.model import KV
 from movs.model import Row
+from movs.postepay import read_postepay
 
 
 def _merge_rows_helper(acc: list[Row], new: list[Row]) -> Iterator[Row]:
@@ -24,11 +26,21 @@ def merge_rows(acc: list[Row], new: list[Row]) -> list[Row]:
 
 
 def read(mov_fn: str) -> tuple[KV, list[Row]]:
-    return (read_estrattoconto if mov_fn.endswith('.pdf') else read_txt)(mov_fn)
+    reader: Callable[[str], tuple[KV, list[Row]]] | None = None
+    if mov_fn.endswith('.txt'):
+        reader = read_txt
+    elif mov_fn == 'ListaMovimenti.pdf':
+        reader = read_postepay
+    elif mov_fn.endswith('.pdf'):
+        reader = read_estrattoconto
+    if reader is None:
+        raise Exception(f'unknown {mov_fn=}')
+
+    return reader(mov_fn)
 
 
 def merge_files(acc_fn: str, *mov_fns: str) -> None:
-    kv, csv = read_txt(acc_fn)
+    kv, csv = read(acc_fn)
     for mov_fn in mov_fns:
         kv, mov_csv = read(mov_fn)
         csv = merge_rows(csv, mov_csv)
@@ -40,6 +52,12 @@ def merge_files(acc_fn: str, *mov_fns: str) -> None:
 def main() -> None:
     if not argv[1:]:
         raise SystemExit(f'uso: {argv[0]} ACCUMULATOR [MOVIMENTI...]')
+
+    if '-h' in argv[1:] or '--help' in argv[1:]:
+        raise SystemExit('''rules for [MOVIMENTI...]:
+        *.txt              -> bpol reader
+        ListaMovimenti.pdf -> postepay
+        *.pdf              -> estrattoconto''')
 
     accumulator, *movimentis = argv[1:]
     merge_files(accumulator, *movimentis)
